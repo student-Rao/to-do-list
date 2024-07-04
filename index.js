0ab3990a8cf756bc3500b2a5a920f1b2830b4f3c
Rao Wakeel <raowakeel895@gmail.com>
2024-07-04 19:29:54 +0500

    this is first time uploaded to-do-list

---
+++ index.js
@@ -0,0 +1,22 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+let list = [];
+let condition = true;
+while (condition) {
+    let todo = await inquirer.prompt([
+        {
+            type: "input",
+            name: "firstthing",
+            message: "what would you like to add some in todo ?"
+        },
+        {
+            type: "confirm",
+            name: "Secondthing",
+            message: "would you like add some in todo (yes/no)",
+            default: "yes"
+        },
+    ]);
+    list.push(todo.firstthing);
+    console.log(list);
+    condition = todo.Secondthing;
+}
